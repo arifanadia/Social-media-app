068060612b777990caf1038d1fecd00516d5c384
arifanadia <contactarifanadia@gmail.com>
2024-09-11 15:00:30 +0600

    appwrite setup and createUser done

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -14,6 +14,7 @@ import { SignupValidation } from '../../lib/validation/index'
 import { z } from "zod"
 import Loader from "@/components/shared/Loader"
 import { Link } from "react-router-dom"
+import { creaateUserAccount } from "@/lib/appwrite/api"
 
 
 const SignupForm = () => {
@@ -32,10 +33,9 @@ const SignupForm = () => {
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    const newUser = await creaateUserAccount(values)
+    console.log(newUser)
   }
   return (
     <div>
