cdfbc4da092503bccdaa4d68684e56325af36bed
arifanadia <contactarifanadia@gmail.com>
2024-09-15 21:00:21 +0600

    done: post form ui

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,13 +1,19 @@
 import { z } from "zod"
 
 export const SignupValidation = z.object({
-    name: z.string().min(2 ,{ message:"too Short"}),
-    username: z.string().min(2 ,{ message:"too Short"}),
-    email : z.string().email(),
-    password : z.string().min(8, {message: "Please must be at least 8 characters long"})
-  })
+  name: z.string().min(2, { message: "too Short" }),
+  username: z.string().min(2, { message: "too Short" }),
+  email: z.string().email(),
+  password: z.string().min(8, { message: "Please must be at least 8 characters long" })
+})
 
 export const SigninValidation = z.object({
-    email : z.string().email(),
-    password : z.string().min(8, {message: "Please must be at least 8 characters long"})
-  })
+  email: z.string().email(),
+  password: z.string().min(8, { message: "Please must be at least 8 characters long" })
+})
+export const PostValidation = z.object({
+  caption: z.string().min(5).max(2200),
+  file: z.custom<File[]>(),
+  location: z.string().min(2).max(100),
+  tags: z.string(),
+})
